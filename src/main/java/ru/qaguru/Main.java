eab79c5cbbc00cbef8662366516d664df8444105
t5582871-svg <dsvredlov@mail.ru>
2025-12-03 23:37:05 +0300

    Add

---
+++ src/main/java/ru/qaguru/Main.java
@@ -0,0 +1,44 @@
+package ru.qaguru;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+        double c = 1.5;
+
+        //Арифметические операции с примитавми int
+        System.out.println("Арифметические операции");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+
+        System.out.println("Арифметических операций над int и double в одном выражении");
+        System.out.println("a + c = " + (a + c));
+        System.out.println("a - c = " + (a - c));
+        System.out.println("a * c = " + (a * c));
+        System.out.println("a - c = " + (a / c));
+
+        //Логические операции с примитавми int
+        System.out.println("Логические операции");
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a >= b = " + (a >= b));
+        System.out.println("a <= b = " + (a <= b));
+
+        //Переполнение при арифметической операции
+        System.out.println("Переполнение при арифметической операции ");
+        int d = Integer.MAX_VALUE;
+        System.out.println("Переполнение Integer " + (d + 1));
+        int e = Integer.MIN_VALUE;
+        System.out.println("Переполнение Integer " + (e - 1));
+        float f = Float.MAX_VALUE;
+        System.out.println("Переполнение Float " + (f * f));
+        double g = Double.MAX_VALUE;
+        System.out.println("Переполнение Double " + (g * g));
+
+    }
+}
